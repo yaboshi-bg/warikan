b0d1cf47ef3d1f0d9452bbf056fbe99731143c7a
n-onoda <n-onoda@biglobe.co.jp>
2020-04-01 18:31:47 +0900

    add base code

---
+++ src/main/java/jp/co/biglobe/warikan/WarikanApplication.java
@@ -0,0 +1,13 @@
+package jp.co.biglobe.warikan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarikanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarikanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/biglobe/warikan/WarikanApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.biglobe.warikan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarikanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
